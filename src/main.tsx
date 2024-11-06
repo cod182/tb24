49d33ca23a22b1651b25d233d9fea1d32e35bebd
Codie Stephens-Evans <codie18@gmail.com>
2024-11-06 09:56:57 +0000

    setup

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
 import './index.css';
 
-import App from './App.tsx'
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+import ReactDOM from 'react-dom/client';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <BrowserRouter>
     <App />
-  </StrictMode>,
-)
+  </BrowserRouter>
+);
