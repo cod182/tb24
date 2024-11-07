61140a2a768db9933a581f02162b5f7844137cd2
Codie Stephens-Evans <codie18@gmail.com>
2024-11-07 14:27:04 +0000

    dashboard header / nav

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,3 +2,4 @@ export { default as AuthForm } from './Auth/AuthForm';
 export { default as ProtectedRoute } from './Auth/ProtectedRoute';
 export { default as DashboardPanel } from './Dashboard/DashboardPanel';
 export { default as DashboardCard } from './Dashboard/DashboardCard';
+export { default as Nav } from './Navigation/Nav';
