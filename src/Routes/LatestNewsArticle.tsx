c915c9e1b0162868c587b2e01458e546fe0bde2f
Codie Stephens-Evans <codie18@gmail.com>
2024-11-08 09:34:44 +0000

    added loading/error loader

--- src/Routes/LatestNewsArticle.tsx
+++ src/Routes/LatestNewsArticle.tsx
@@ -1,8 +1,9 @@
+import { BiError, BiWorld } from 'react-icons/bi';
 import { Loader, NewsArticle } from '../components';
 import { useEffect, useState } from 'react';
 
-import { BiWorld } from 'react-icons/bi';
 import { FeedItem } from '../../types/custom';
+import bgImage from '../assets/media/images/dash-bg.webp';
 import { fetchBBCRSSFeed } from '../utils/functions';
 
 const LatestNewsArticle = () => {
@@ -35,13 +36,25 @@ const LatestNewsArticle = () => {
 		fetchRSSFeed('https://feeds.bbci.co.uk/news/rss.xml');
 	}, []);
 
-	if (!loading) return (<Loader title='Loading News...' subText='Please wait...' icon={BiWorld} />)
+
 
 	// if (error) return (<Error />)
 
 	return (
-		<div>
-			{/* <NewsArticle article={feedItems[0]} /> */}
+		<div className='w-full min-h-[100dvh] relativ flex flex-col items-center justify-center'>
+			{/* background */}
+			<img src={bgImage} alt="background" className='absolute top-0 left-0 select-none h-full w-full z-[-1] blur-sm object-cover' />
+
+			{error ? (
+				<Loader title='Error!' subText={error} icon={BiError} />
+			) : !loading ? (
+				<div className='w-full h-full flex flex-col items-center justify-center'>
+					<Loader title='Loading Latest News!' subText='Please wait...' icon={BiWorld} />
+				</div>
+			) : (
+
+				<NewsArticle article={feedItems[0]} />
+			)}
 		</div>
 	)
 
