77b2c90159156dd35cbfc1b4c426af810e082fbd
Codie Stephens-Evans <codie18@gmail.com>
2024-11-08 23:53:53 +0000

    update blob

--- netlify/functions/fetch-rss.js
+++ netlify/functions/fetch-rss.js
@@ -38,3 +38,30 @@ export const handler = async (event, context) => {
 		};
 	}
 };
+
+// import fetch from 'node-fetch';
+
+// export const handler = async (event, context) => {
+// 	const rssFeedUrl = 'https://feeds.bbci.co.uk/news/rss.xml';
+
+// 	try {
+// 		const response = await fetch(rssFeedUrl);
+// 		if (!response.ok) {
+// 			return {
+// 				statusCode: 500,
+// 				body: JSON.stringify({ message: 'Error fetching RSS feed', status: response.status }),
+// 			};
+// 		}
+
+// 		const data = await response.text();
+// 		return {
+// 			statusCode: 200,
+// 			body: data,
+// 		};
+// 	} catch (error) {
+// 		return {
+// 			statusCode: 500,
+// 			body: JSON.stringify({ message: 'Failed to fetch RSS feed', error: error.message }),
+// 		};
+// 	}
+// };
