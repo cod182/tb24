9b59633c2a1abf17b018d8187cb5e641e556bce3
Codie Stephens-Evans <codie18@gmail.com>
2024-11-09 01:06:33 +0000

    testing axios in netllify

--- netlify/functions/fetch-tbox.js
+++ netlify/functions/fetch-tbox.js
@@ -1,34 +1,62 @@
-import fetch from 'node-fetch';
+// import fetch from 'node-fetch';
+
+// export const handler = async (event, context) => {
+// 	const rssFeedUrl = 'https://tboxapps.therapy-box.co.uk/hackathon/clothing-api.php?username=swapnil';
+
+// 	try {
+// 		const response = await fetch(rssFeedUrl);
+
+// 		if (!response.ok) {
+// 			return {
+// 				statusCode: 500,
+// 				body: JSON.stringify({ message: 'Error fetching RSS feed' }),
+// 				headers: {
+// 					'Access-Control-Allow-Origin': '*',
+// 					'Content-Type': 'application/json',
+// 				},
+// 			};
+// 		}
+
+// 		const data = await response.text();
+
+// 		return {
+// 			statusCode: 200,
+// 			body: data,
+// 			headers: {
+// 				'Access-Control-Allow-Origin': '*',
+// 				'Content-Type': 'application/json',
+// 			},
+// 		};
+// 	} catch (error) {
+// 		console.error('Error fetching RSS feed:', error);
+// 		return {
+// 			statusCode: 500,
+// 			body: JSON.stringify({ message: 'Failed to fetch RSS feed', error: error.message }),
+// 			headers: {
+// 				'Access-Control-Allow-Origin': '*',
+// 			},
+// 		};
+// 	}
+// };
+
+import axios from 'axios';
 
 export const handler = async (event, context) => {
 	const rssFeedUrl = 'https://tboxapps.therapy-box.co.uk/hackathon/clothing-api.php?username=swapnil';
 
 	try {
-		const response = await fetch(rssFeedUrl);
-
-		if (!response.ok) {
-			return {
-				statusCode: 500,
-				body: JSON.stringify({ message: 'Error fetching RSS feed' }),
-				headers: {
-					'Access-Control-Allow-Origin': '*',
-					'Content-Type': 'application/json',
-				},
-			};
-		}
-
-		const data = await response.text();
+		const response = await axios.get(rssFeedUrl);
 
 		return {
 			statusCode: 200,
-			body: data,
+			body: response.data,
 			headers: {
 				'Access-Control-Allow-Origin': '*',
 				'Content-Type': 'application/json',
 			},
 		};
 	} catch (error) {
-		console.error('Error fetching RSS feed:', error);
+		console.error('Error fetching RSS feed:', error.message);
 		return {
 			statusCode: 500,
 			body: JSON.stringify({ message: 'Failed to fetch RSS feed', error: error.message }),
