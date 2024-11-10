9284f82a579c31f5fb13a06f4a6f55b5a3192243
Codie Stephens-Evans <codie18@gmail.com>
2024-11-10 21:46:05 +0000

    created sports-results page and route

---
+++ src/Routes/SportResults.tsx
@@ -0,0 +1,67 @@
+import { BiError, BiWorld } from 'react-icons/bi';
+import { Loader, PageTitleWithNav } from '../components';
+import { useEffect, useState } from 'react';
+
+import { SportType } from '../../types/custom';
+import bgImage from '../assets/media/images/dash-bg.webp';
+import { fetchSportData } from '../utils/functions';
+
+const SportResults = () => {
+	const [sportItems, setSportItems] = useState<SportType[]>([]);
+	const [loading, setLoading] = useState(true);
+	const [error, setError] = useState<string>();
+
+	useEffect(() => {
+		const fetchSportsData = async () => {
+			setLoading(true);
+			try {
+				const response: SportType[] = await fetchSportData();
+
+				if (!response) {
+					setError('Error Fetching Sport Items');
+					setLoading(false);
+					return;
+				}
+				setLoading(false);
+				setSportItems(response);
+
+			} catch (error) {
+				setLoading(false);
+				setError('Error Fetching Sport Items');
+				console.error('Failed to fetch Sport Items:', error);
+			}
+		};
+
+
+		fetchSportsData();
+	}, []);
+
+	return (
+		<div className='w-full min-h-[100dvh] relative p-4 sm:p-24 flex flex-col items-center justify-start gap-5'>
+			{/* background */}
+			<img src={bgImage} alt="background" className='absolute top-0 left-0 select-none h-full w-full z-[-1] blur-sm object-cover' />
+
+			<PageTitleWithNav title='Sport Results' navLink='/dashboard' titleStyles='text-5xl text-white capitalize md:text-7xl text-start' />
+
+			{error ? (
+				<Loader title='Error!' subText={error} icon={BiError} />
+			) : loading ? (
+				<div className='flex flex-col items-center justify-center w-full h-full'>
+					<Loader title='Loading Latest News!' subText='Please wait...' icon={BiWorld} />
+				</div>
+			) : sportItems.length > 0 ? (
+
+				<div>
+					{/* COMPONENT FOR SPORT SEARCH */}
+				</div>
+
+			) : (
+				<div className="w-full max-w-md text-center text-white">
+					<Loader title='No Sports Results Available!' subText='Please wait...' icon={BiWorld} />
+				</div>
+			)}
+		</div>
+	);
+};
+
+export default SportResults;
--- src/components/Dashboard/SportCard.tsx
+++ src/components/Dashboard/SportCard.tsx
@@ -1,7 +1,8 @@
-import { BiError, BiWorld } from "react-icons/bi";
 import { useEffect, useState } from "react";
 
+import { BiError } from "react-icons/bi";
 import DashboardCard from "./DashboardCard"
+import { GiSoccerBall } from "react-icons/gi";
 import Loader from "../Loader";
 import { SportType } from "../../../types/custom";
 import { fetchSportData } from "../../utils/functions";
@@ -46,12 +47,12 @@ const SportCard = () => {
 
 
 	return (
-		<DashboardCard title='Sport' link='/sport-results' ariaLabel='Read more'>
+		<DashboardCard title='Sport' link='/sports-results' ariaLabel='Read more'>
 			<div className="flex flex-col items-center justify-start w-full h-full gap-2 py-2">
 				{error ? (
 					<Loader title="Error!" subText={error} icon={BiError} />
 				) : loading ? (
-					<Loader title="Loading News" subText="Please wait..." icon={BiWorld} />
+					<Loader title="Loading Results" subText="Please wait..." icon={GiSoccerBall} />
 				) : randomMatch ? (
 					<>
 						<div className="flex flex-col items-center justify-center w-full h-fit">
@@ -59,10 +60,12 @@ const SportCard = () => {
 							<p className="text-sm italic text-gray-700">{randomMatch.date}</p>
 						</div>
 						<div className="flex flex-col items-center justify-center gap-2">
-							<p className="w-full text-2xl text-center">Congratulations to <span className="font-semibold">{randomMatch.homeTeam}</span> for
-								{(randomMatch.fullTimeAwayGoals === randomMatch.fullTimeHomeGoals) ? (randomMatch.fullTimeHomeGoals < 1) ? 'drawing with' : `their win against` : 'their game against'}
-								{randomMatch.awayTeam}</p>
-							<p className="w-full text-xl font-bold text-center">Result: <span className="font-bold">{randomMatch.fullTimeHomeGoals} - {randomMatch.fullTimeAwayGoals}</span></p>
+							<p className="w-full text-xl text-center">Congratulations to <span className="font-semibold">{randomMatch.homeTeam}</span> for&nbsp;{randomMatch.fullTimeHomeGoals > randomMatch.fullTimeAwayGoals
+								? 'their win against'
+								: randomMatch.fullTimeHomeGoals < randomMatch.fullTimeAwayGoals
+									? 'their game against'
+									: 'drawing with'}&nbsp;{randomMatch.awayTeam}</p>
+							<p className="w-full text-xl text-center">Result: <span className="font-semibold">{randomMatch.fullTimeHomeGoals} - {randomMatch.fullTimeAwayGoals}</span></p>
 						</div>
 
 					</>
