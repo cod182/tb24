2cb8006786446a497bdf0432d103527f1aef55d3
Codie Stephens-Evans <codie18@gmail.com>
2024-11-11 16:59:53 +0000

    transition added to taskpage

--- src/Routes/Tasks.tsx
+++ src/Routes/Tasks.tsx
@@ -2,6 +2,7 @@ import { BiError, BiWorld } from 'react-icons/bi';
 import { Loader, PageTitleWithNav } from '../components';
 
 import AddTaskLineFull from '../components/Tasks/AddTaskLineFull';
+import { Slide } from 'react-awesome-reveal';
 import TaskLineFull from '../components/Tasks/TaskLineFull';
 import bgImage from '../assets/media/images/dash-bg.webp';
 import { useTaskContext } from '../context/useTaskContext';
@@ -10,7 +11,7 @@ const Tasks = () => {
 	const { tasks, loading, error } = useTaskContext();
 
 	return (
-		<div className='w-full min-h-[100dvh] relative p-4 md:p-24'>
+		<div className='w-full min-h-[100dvh] relative p-4 md:p-24 overflow-hidden'>
 			{/* background */}
 			<img src={bgImage} alt="background" className='absolute top-0 left-0 select-none h-full w-full z-[-1] blur-sm object-cover' />
 			{/* Page Title */}
@@ -24,11 +25,16 @@ const Tasks = () => {
 				</div>
 			) : (
 				<section className='flex flex-col items-center justify-center w-full h-full gap-20 py-4 mx-auto mt-20 pe-12 lg:px-24 grow'>
-					{tasks?.map((task) => (
-						<TaskLineFull task={task} key={task.$id} />
-					))}
 
-					<AddTaskLineFull />
+					<Slide direction='left' className='relative w-full' cascade>
+						{tasks?.map((task) => (
+							<TaskLineFull task={task} key={task.$id} />
+						))}
+
+						<AddTaskLineFull />
+
+
+					</Slide>
 				</section>
 			)}
 		</div>
